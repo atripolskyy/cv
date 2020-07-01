fa8694a2ed7be440f526f6f4aa869fe10e9263ba
Oleksii Trypolskyi <atripolskyy@gmail.com>
2020-07-01 13:43:25 +0300

    Updates

---
+++ precache-manifest.2a778a0e762244f9a9148872986563ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0c68c3ab02fc09171b7a5e5986facf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c5b3f04c8279047c8b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "095c039d018d2581864a",
+    "url": "/static/js/2.16a53062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16a53062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c5b3f04c8279047c8b",
+    "url": "/static/js/main.24871637.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
